97c8ca4a9fba69ee4b35915d60cb3ce19f10c293
Oscar Söderlund <odsod@users.noreply.github.com>
2021-08-05 20:11:42 +0200

    feat: add cloudpubsub.NewHTTPHandler

---
+++ cloudpubsub/httphandler_test.go
@@ -0,0 +1,55 @@
+package cloudpubsub
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"google.golang.org/genproto/googleapis/pubsub/v1"
+	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"gotest.tools/v3/assert"
+)
+
+func TestNewHTTPHandler(t *testing.T) {
+	// From: https://cloud.google.com/pubsub/docs/push#receiving_messages
+	const example = `
+		{
+			"message": {
+				"attributes": {
+					"key": "value"
+				},
+				"data": "SGVsbG8gQ2xvdWQgUHViL1N1YiEgSGVyZSBpcyBteSBtZXNzYWdlIQ==",
+				"messageId": "2070443601311540",
+				"message_id": "2070443601311540",
+				"publishTime": "2021-02-26T19:13:55.749Z",
+				"publish_time": "2021-02-26T19:13:55.749Z"
+			},
+		   "subscription": "projects/myproject/subscriptions/mysubscription"
+		}
+	`
+	expectedMessage := &pubsub.PubsubMessage{
+		Data:       []byte("Hello Cloud Pub/Sub! Here is my message!"),
+		Attributes: map[string]string{"key": "value"},
+		MessageId:  "2070443601311540",
+		PublishTime: &timestamppb.Timestamp{
+			Seconds: 1614366835,
+			Nanos:   749000000,
+		},
+	}
+	var actualMessage *pubsub.PubsubMessage
+	fn := func(ctx context.Context, message *pubsub.PubsubMessage) error {
+		actualMessage = message
+		return nil
+	}
+	server := httptest.NewServer(NewHTTPHandler(fn))
+	defer server.Close()
+	request, err := http.NewRequest(http.MethodPost, server.URL, strings.NewReader(example))
+	assert.NilError(t, err)
+	response, err := http.DefaultClient.Do(request)
+	assert.NilError(t, err)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	assert.DeepEqual(t, expectedMessage, actualMessage, protocmp.Transform())
+}
---
+++ cloudstatus/code.go
@@ -0,0 +1,52 @@
+package cloudstatus
+
+import (
+	"net/http"
+
+	"google.golang.org/grpc/codes"
+)
+
+// ToHTTP converts a gRPC error code into the corresponding HTTP response status.
+// See: https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto
+// From: https://github.com/grpc-ecosystem/grpc-gateway/blob/master/runtime/errors.go
+func ToHTTP(code codes.Code) int {
+	switch code {
+	case codes.OK:
+		return http.StatusOK
+	case codes.Canceled:
+		return http.StatusRequestTimeout
+	case codes.Unknown:
+		return http.StatusInternalServerError
+	case codes.InvalidArgument:
+		return http.StatusBadRequest
+	case codes.DeadlineExceeded:
+		return http.StatusGatewayTimeout
+	case codes.NotFound:
+		return http.StatusNotFound
+	case codes.AlreadyExists:
+		return http.StatusConflict
+	case codes.PermissionDenied:
+		return http.StatusForbidden
+	case codes.Unauthenticated:
+		return http.StatusUnauthorized
+	case codes.ResourceExhausted:
+		return http.StatusTooManyRequests
+	case codes.FailedPrecondition:
+		// This deliberately doesn't translate to the similarly named '412 Precondition Failed' HTTP response status.
+		return http.StatusBadRequest
+	case codes.Aborted:
+		return http.StatusConflict
+	case codes.OutOfRange:
+		return http.StatusBadRequest
+	case codes.Unimplemented:
+		return http.StatusNotImplemented
+	case codes.Internal:
+		return http.StatusInternalServerError
+	case codes.Unavailable:
+		return http.StatusServiceUnavailable
+	case codes.DataLoss:
+		return http.StatusInternalServerError
+	default:
+		return http.StatusInternalServerError
+	}
+}
