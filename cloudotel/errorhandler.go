b79f824faa9009e194a63f86297e022123baa91a
Eric Wennerberg <ericwenn@users.noreply.github.com>
2022-11-08 20:18:50 +0100

    refactor(cloudotel): move error handler from metric exporter
    This handler handles error both from tracing and metrics.

--- cloudotel/errorhandler.go
+++ cloudotel/errorhandler.go
@@ -1,6 +1,9 @@
 package cloudotel
 
 import (
+	"context"
+
+	"go.einride.tech/cloudrunner/cloudzap"
 	"go.opentelemetry.io/otel"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -20,4 +23,27 @@ type errorHandler struct {
 // Handle implements otel.ErrorHandler.
 func (e errorHandler) Handle(err error) {
 	e.logger.Check(e.level, e.message).Write(zap.Error(err))
+}
+
+// RegisterErrorHandler registers a global OpenTelemetry error handler.
+func RegisterErrorHandler(ctx context.Context) {
+	otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+		handleError(ctx, err)
+	}))
+}
+
+func handleError(ctx context.Context, err error) {
+	if isUnsupportedSamplerErr(err) {
+		// The OpenCensus bridge does not support all features from OpenCensus,
+		// for example custom samplers which is used in some libraries.
+		// The bridge presumably falls back to the configured sampler, so
+		// this error can be ignored.
+		//
+		// See
+		// https://pkg.go.dev/go.opentelemetry.io/otel/bridge/opencensus
+		return
+	}
+	if logger, ok := cloudzap.GetLogger(ctx); ok {
+		logger.Warn("otel error", zap.Error(err))
+	}
 }
