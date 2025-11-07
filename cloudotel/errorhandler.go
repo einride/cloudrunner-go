a88eaebc4cadfc028b84aa9b6c8282a033a6e44f
Fredrik Averpil <fredrik.averpil@einride.tech>
2025-11-07 12:18:11 +0100

    feat!: remove deprecated no-op

--- cloudotel/errorhandler.go
+++ cloudotel/errorhandler.go
@@ -5,15 +5,7 @@ import (
 	"log/slog"
 
 	"go.opentelemetry.io/otel"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
-
-// NewErrorLogger returns a new otel.ErrorHandler that logs errors using the provided logger, level and message.
-// Deprecated: This is a no-op as part of the migration from zap to slog.
-func NewErrorLogger(*zap.Logger, zapcore.Level, string) otel.ErrorHandler {
-	return otel.ErrorHandlerFunc(func(error) {})
-}
 
 // RegisterErrorHandler registers a global OpenTelemetry error handler.
 func RegisterErrorHandler(ctx context.Context) {
